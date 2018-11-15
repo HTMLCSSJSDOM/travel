c6a4eb94bb2b7e0d532e3ab5caebd4b54c2c1ff9
huanghaoming <huanghaoming@jingyu.com>
2018-11-15 20:25:28 +0800

    icons页面

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -2,16 +2,18 @@
 	<div>
 		
 		<home-header></home-header>
-	
+		<home-icon></home-icon>
 	</div>
 </template>
 
 <script type="text/javascript">
 	import header from './components/header'
+	import homeIcon from './components/Icons.vue'
 	export default {
 		name: 'home',
 		components: {
-			'home-header': header
+			'home-header': header,
+			'home-icon': homeIcon
 		},
 		data(){
 			return {
